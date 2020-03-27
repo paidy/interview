53344b2f94b7660984f343add066b19c5bf2d1c6
Cédric Chantepie <cchantep@users.noreply.github.com>
2020-03-27 10:57:29 +0100

    Update the build and dependencies (#18)

--- forex-mtl/src/main/scala/forex/config/Config.scala
+++ forex-mtl/src/main/scala/forex/config/Config.scala
@@ -2,11 +2,18 @@ package forex.config
 
 import cats.effect.Sync
 import fs2.Stream
+
+import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 
 object Config {
 
-  def stream[F[_]: Sync](path: String): Stream[F, ApplicationConfig] =
-    Stream.eval(Sync[F].delay(pureconfig.loadConfigOrThrow[ApplicationConfig](path)))
+  /**
+   * @param path the property path inside the default configuration
+   */
+  def stream[F[_]: Sync](path: String): Stream[F, ApplicationConfig] = {
+    Stream.eval(Sync[F].delay(
+      ConfigSource.default.at(path).loadOrThrow[ApplicationConfig]))
+  }
 
 }

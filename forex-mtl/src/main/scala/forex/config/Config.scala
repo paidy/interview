587a8d5664a21a33071fef232a9a8cbdae7404e9
b3nk3i <rom1.petit@gmail.com>
2022-06-09 21:24:00 +0200

    update typelevel stack

--- forex-mtl/src/main/scala/forex/config/Config.scala
+++ forex-mtl/src/main/scala/forex/config/Config.scala
@@ -2,18 +2,15 @@ package forex.config
 
 import cats.effect.Sync
 import fs2.Stream
-
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 
 object Config {
 
   /**
-   * @param path the property path inside the default configuration
-   */
-  def stream[F[_]: Sync](path: String): Stream[F, ApplicationConfig] = {
-    Stream.eval(Sync[F].delay(
-      ConfigSource.default.at(path).loadOrThrow[ApplicationConfig]))
-  }
+    * @param path the property path inside the default configuration
+    */
+  def stream[F[_]: Sync](path: String): Stream[F, ApplicationConfig] =
+    Stream.eval(Sync[F].delay(ConfigSource.default.at(path).loadOrThrow[ApplicationConfig]))
 
 }

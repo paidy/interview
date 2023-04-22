5845853fdd23cc3810f965c2f86828441a0f1f77
hiramekun <thescript1210@gmail.com>
2023-04-23 04:01:03 +0900

    feat: OneFrameClient

---
+++ forex-mtl/src/main/scala/forex/http/external/oneframe/OneFrameHttpClient.scala
@@ -0,0 +1,26 @@
+package forex.http.external.oneframe
+
+import cats.ApplicativeError
+import cats.effect.Sync
+import cats.implicits.catsSyntaxApplicativeError
+import forex.domain.model.Rate
+import forex.http.external.oneframe.Protocol.RateResponse
+import org.http4s.client.Client
+import org.http4s.{Header, Method, Request, Uri}
+
+class OneFrameHttpClient[F[_]: Sync](token: String, httpClient: Client[F])(
+    implicit applicativeError: ApplicativeError[F, Throwable]
+) extends OneFrameClient[F] {
+
+  private val baseUri   = Uri.unsafeFromString("http://localhost:8080")
+  private val authToken = Header("token", token)
+
+  def getRates(pairs: Seq[Rate.Pair]): F[List[RateResponse]] = {
+    val uri     = baseUri.withPath("/rates").withQueryParam("pair", pairs.mkString("&pair="))
+    val request = Request[F](Method.GET, uri).withHeaders(authToken)
+
+    httpClient.expect[List[RateResponse]](request).handleErrorWith { e =>
+      applicativeError.raiseError(new Exception(s"Error fetching rates: ${e.getMessage}", e))
+    }
+  }
+}

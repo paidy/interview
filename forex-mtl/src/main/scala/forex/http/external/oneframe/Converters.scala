a26d0d38d2c7f135445ed3a31303e0ec56649a7e
hiramekun <thescript1210@gmail.com>
2023-04-23 05:57:53 +0900

    fix: datetime formatter

--- forex-mtl/src/main/scala/forex/http/external/oneframe/Converters.scala
+++ forex-mtl/src/main/scala/forex/http/external/oneframe/Converters.scala
@@ -4,14 +4,17 @@ import forex.domain.model.{ Currency, Price, Rate, Timestamp }
 import forex.http.external.oneframe.Protocol.RateResponse
 
 import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 object Converters {
+
+  private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX")
 
   private[oneframe] implicit class RateOps(val rateResponse: RateResponse) extends AnyVal {
     def asRate: Rate = Rate(
       pair = Rate.Pair(Currency.fromString(rateResponse.from), Currency.fromString(rateResponse.to)),
       price = Price(rateResponse.price),
-      timestamp = Timestamp(OffsetDateTime.parse(rateResponse.time_stamp))
+      timestamp = Timestamp(OffsetDateTime.parse(rateResponse.time_stamp + "Z", formatter))
     )
   }
 }

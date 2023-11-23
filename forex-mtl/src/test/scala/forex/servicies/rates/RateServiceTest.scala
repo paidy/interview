5821e72e7e34a2a78f9f7fe2b292b4c56a960836
Minsu Roh <minsu.roh@MacBook-Pro.local>
2023-11-23 15:36:53 +0900

    add pseudo tests

--- forex-mtl/src/test/scala/forex/servicies/rates/RateServiceTest.scala
+++ forex-mtl/src/test/scala/forex/servicies/rates/RateServiceTest.scala
@@ -5,17 +5,43 @@ import forex.domain._
 import forex.services.rates.interpreters.OneFrameInterpreter
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.http4s.client.Client
+import org.http4s.{HttpApp, Response, Status, Uri}
+import org.http4s.circe._
+import io.circe.syntax._
+import io.circe.generic.auto._
 
 class RateServiceTest extends AnyFlatSpec with Matchers {
   "RateService" should "retrieve a valid rate for a currency pair" in {
-    val service = new OneFrameInterpreter[IO]()
-    val result = service.get(Rate.Pair(Currency.USD, Currency.JPY)).unsafeRunSync()
+    val expectedRateResponse = RateResponse(
+      from = Currency.USD,
+      to = Currency.JPY,
+      bid = BigDecimal(103.5),
+      ask = BigDecimal(104.0),
+      price = Price(BigDecimal(103.75)),
+      timeStamp = Timestamp.now
+    )
 
-    result should be a "Right"
-    result.foreach { rate =>
-      rate.pair.from shouldBe Currency.USD
-      rate.pair.to shouldBe Currency.JPY
-      rate.price.value should be > 0
+    val stubClient: Client[IO] = Client.fromHttpApp[IO](
+      HttpApp[IO] {
+        case _ =>
+          IO(Response[IO](status = Status.Ok).withEntity(expectedRateResponse.asJson))
+      }
+    )
+
+    val oneFrameUri = Uri.uri("http://localhost/rates")
+    val apiToken = "dummyToken"
+
+    val service = new OneFrameInterpreter[IO](oneFrameUri, apiToken)
+    val result = service.get(List(Rate.Pair(Currency.USD, Currency.JPY))).unsafeRunSync()
+
+    result should matchPattern { case Right(_) => }
+    result.foreach { rates =>
+      rates.headOption.foreach { rateResponse =>
+        rateResponse.from shouldBe Currency.USD
+        rateResponse.to shouldBe Currency.JPY
+        rateResponse.price.value should be > BigDecimal(0)
+      }
     }
   }
 }

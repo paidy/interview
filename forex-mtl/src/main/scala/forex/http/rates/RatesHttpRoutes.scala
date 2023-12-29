cf079534794c3994143293d2bb4ce77c1a593334
Sushant Adhikari <sushant.adhikari@myraindrop.co.uk>
2023-12-30 02:08:45 +0530

    add validation for currency

--- forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
+++ forex-mtl/src/main/scala/forex/http/rates/RatesHttpRoutes.scala
@@ -1,11 +1,12 @@
 package forex.http
 package rates
 
+import cats.data.Validated.Valid
 import cats.effect.Sync
 import cats.syntax.flatMap._
 import forex.programs.RatesProgram
 import forex.programs.rates.{ Protocol => RatesProgramProtocol }
-import org.http4s.HttpRoutes
+import org.http4s.{ HttpRoutes, Response, Status }
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 
@@ -17,8 +18,16 @@ class RatesHttpRoutes[F[_]: Sync](rates: RatesProgram[F]) extends Http4sDsl[F] {
 
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root :? FromQueryParam(from) +& ToQueryParam(to) =>
-      rates.get(RatesProgramProtocol.GetRatesRequest(from, to)).flatMap(Sync[F].fromEither).flatMap { rate =>
-        Ok(rate.asGetApiResponse)
+      (from, to) match {
+        case (Valid(from), Valid(to)) =>
+          rates
+            .get(RatesProgramProtocol.GetRatesRequest(from, to))
+            .flatMap {
+              case Left(err) =>
+                Sync[F].pure(Response[F]().withStatus(Status.InternalServerError).withEntity(err.getMessage))
+              case Right(rate) => Ok(rate.asGetApiResponse)
+            }
+        case (_, _) => BadRequest("Currency not supported.")
       }
   }
 
--- forex-mtl/src/test/scala/forex/services/rates/interpreters/OneFrameServiceSpec.scala
+++ forex-mtl/src/test/scala/forex/services/rates/interpreters/OneFrameServiceSpec.scala
@@ -18,36 +18,39 @@ import scala.concurrent.duration.DurationInt
 class OneFrameServiceSpec extends AnyFunSuite with Matchers {
 
   implicit val contextShiftInstance: ContextShift[IO] = IO.contextShift(scala.concurrent.ExecutionContext.global)
-  implicit val timerInstance: Timer[IO] = IO.timer(scala.concurrent.ExecutionContext.global)
+  implicit val timerInstance: Timer[IO]               = IO.timer(scala.concurrent.ExecutionContext.global)
 
   // Test case
   test("get should return cached rate if available") {
     // Mock OneFrameClient for testing
     class MockOneFrameClient[F[_]: Applicative] extends OneFrameClient[F] {
       override def getRates(
-                             pairs: Vector[Rate.Pair]
-                           ): F[Either[Error, List[OneFrameCurrencyInformation]]] = {
-        val mockResponse = List(OneFrameCurrencyInformation(from = "USD",
-          to= "JPY",
-          bid= 0.4332159351433372,
-          ask= 0.2056211718073755,
-          price= 0.31941855347535635,
-          time_stamp= "2023-12-29T17:22:46.691Z"))
+          pairs: Vector[Rate.Pair]
+      ): F[Either[Error, List[OneFrameCurrencyInformation]]] = {
+        val mockResponse = List(
+          OneFrameCurrencyInformation(
+            from = "USD",
+            to = "JPY",
+            bid = 0.4332159351433372,
+            ask = 0.2056211718073755,
+            price = 0.31941855347535635,
+            time_stamp = "2023-12-29T17:22:46.691Z"
+          )
+        )
 
         Applicative[F].pure(mockResponse.asRight)
       }
     }
     val oneFrameClient = new MockOneFrameClient[IO]
-    val cache = CaffeineCache[Rate]
-    val cacheConfig = CacheConfig(5)
-    val service = new OneFrameService[IO](oneFrameClient, cache, cacheConfig)
+    val cache          = CaffeineCache[Rate]
+    val cacheConfig    = CacheConfig(5)
+    val service        = new OneFrameService[IO](oneFrameClient, cache, cacheConfig)
 
     val pair = Rate.Pair(Currency.USD, Currency.EUR)
     val rate = Rate(pair, Price(BigDecimal(1.2)), Timestamp(OffsetDateTime.now()))
 
-
     // Put rate into the cache
-    cache.put(pair.key)(rate, 5.minutes.some)
+    cache.put(pair.key)(rate, cacheConfig.oneFrameExpiry.minutes.some)
 
     val result = service.get(pair).unsafeRunSync()
 
@@ -59,17 +62,17 @@ class OneFrameServiceSpec extends AnyFunSuite with Matchers {
     // Mock OneFrameClient for testing
     class MockOneFrameClientNegative[F[_]: Applicative] extends OneFrameClient[F] {
       override def getRates(
-                             pairs: Vector[Rate.Pair]
-                           ): F[Either[Error, List[OneFrameCurrencyInformation]]] = {
+          pairs: Vector[Rate.Pair]
+      ): F[Either[Error, List[OneFrameCurrencyInformation]]] = {
         val leftResponse = Error.OneFrameLookupFailed("Something went wrong...")
         Applicative[F].pure(leftResponse.asLeft)
       }
     }
 
     val oneFrameClientNegative = new MockOneFrameClientNegative[IO]
-    val cacheNegative = CaffeineCache[Rate]
-    val cacheConfigNegative = CacheConfig(5)
-    val service = new OneFrameService[IO](oneFrameClientNegative, cacheNegative, cacheConfigNegative)
+    val cacheNegative          = CaffeineCache[Rate]
+    val cacheConfigNegative    = CacheConfig(5)
+    val service                = new OneFrameService[IO](oneFrameClientNegative, cacheNegative, cacheConfigNegative)
 
     val pairNegative = Rate.Pair(Currency.USD, Currency.EUR)
 

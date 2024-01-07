9ea33d5d6ac7d51c90b85b9dc166a288d4c5bbb1
anuar.alibekov <anuar.alibekov@glidewelldental.com>
2024-01-07 14:29:25 +0600

    fix

--- forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
+++ forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
@@ -6,8 +6,6 @@ import interpreters._
 import sttp.client3.SttpBackend
 
 object Interpreters {
-  def dummy[F[_]: Applicative]: Algebra[F] = new OneFrameDummy[F]()
-
   def live[F[_]: Monad](config: ProviderConfig, backend: SttpBackend[F, _]): Algebra[F] =
     new OneFrameLive[F](config, backend)
 }

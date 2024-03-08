85c5f99f262834a4cb8fbbfe2d94074ae7c4d0f1
Billy Yuan <billy.yuan1@gmail.com>
2024-03-09 07:55:27 +0900

    Get error message from One Frame API

--- forex-mtl/src/main/scala/forex/thirdPartyApi/oneFrame/OneFrameApiClient.scala
+++ forex-mtl/src/main/scala/forex/thirdPartyApi/oneFrame/OneFrameApiClient.scala
@@ -12,42 +12,52 @@ import sttp.client4.quick._
 import sttp.model.Uri
 import errors._
 
-final case class RateDto(from: String, to: String, bid: BigDecimal, ask: BigDecimal, price: BigDecimal, time_stamp: String)
+final case class RateSchema(from: String, to: String, bid: BigDecimal, ask: BigDecimal, price: BigDecimal, time_stamp: String)
+final case class ErrorResponse(error: String)
 
-object RateDto {
-  implicit final val RateDtoCodec: Codec[RateDto] = deriveCodec
+object RateSchema {
+  implicit final val RateDtoCodec: Codec[RateSchema] = deriveCodec
+}
+
+object ErrorResponse {
+	implicit final val ApiErrorResponseCodec: Codec[ErrorResponse] = deriveCodec
 }
 
 class OneFrameApiClient {
   def getAll(): Either[OneFrameApiClientError, Map[Rate.Pair, Rate]] = {
-	// Build URL and add currency pairs as query parameters 
-	var url: Uri = config.endpoint.get()
-	this.getCurrencyPairs()
-	.foreach((pair) => {
-		url = url.addParam("pair", pair.from.toString() + pair.to.toString())
-	})
+		// Build URL and add currency pairs as query parameters 
+		var url: Uri = config.endpoint.get()
+		this.getCurrencyPairs()
+		.foreach((pair) => {
+			url = url.addParam("pair", pair.from.toString() + pair.to.toString())
+		})
 
-	// Send request
-	val response = quickRequest.get(url).header("token", config.token.get()).send()
+		// Send request
+		val response = quickRequest.get(url).header("token", config.token.get()).send()
 		
-	// Convert response body to RateDtos
-	val maybeRateDtos = decode[List[RateDto]](response.body) match {
-		case Left(_) => Left(errors.OneFrameApiClientError.JsonDecodingError("Error decoding JSON"))
-		case Right(i) => Right(i)
-	}
-
-	var rateMap: Map[Rate.Pair, Rate] = new HashMap()
-
-	maybeRateDtos.fold(
-		e => Left(e),
-		rateDtos => {
-			for (rateDto <- rateDtos) {
-				val ratePair = Rate.Pair(Currency.fromString(rateDto.from), Currency.fromString(rateDto.to))
-				val rate = Rate(ratePair, Price(rateDto.price), Timestamp.fromString(rateDto.time_stamp))
-				rateMap = rateMap + (ratePair -> rate)
-			}
-			Right(rateMap)
+		// Convert response body to RateDtos
+		val maybeRateDtos = decode[List[RateSchema]](response.body) match {
+			case Left(_) => 
+				decode[ErrorResponse](response.body) match {
+					case Left(_) => Left(errors.OneFrameApiClientError.JsonDecodingError("Could not decode JSON"))
+					case Right(r) => Left(errors.OneFrameApiClientError.RequestError(r.error))
+				}
+				
+			case Right(i) => Right(i)
 		}
+
+		var rateMap: Map[Rate.Pair, Rate] = new HashMap()
+
+		maybeRateDtos.fold(
+			e => Left(e),
+			rateDtos => {
+				for (rateDto <- rateDtos) {
+					val ratePair = Rate.Pair(Currency.fromString(rateDto.from), Currency.fromString(rateDto.to))
+					val rate = Rate(ratePair, Price(rateDto.price), Timestamp.fromString(rateDto.time_stamp))
+					rateMap = rateMap + (ratePair -> rate)
+				}
+				Right(rateMap)
+			}
 		)
   }
 
--- forex-mtl/src/main/scala/forex/thirdPartyApi/oneFrame/errors.scala
+++ forex-mtl/src/main/scala/forex/thirdPartyApi/oneFrame/errors.scala
@@ -2,10 +2,13 @@ package forex.thirdPartyApi.oneFrame
 
 object errors {
 
-  sealed trait OneFrameApiClientError
+  sealed trait OneFrameApiClientError {
+    val msg: String
+  }
 
   object OneFrameApiClientError {
     final case class JsonDecodingError(msg: String) extends OneFrameApiClientError
+    final case class RequestError(msg: String) extends OneFrameApiClientError
   }
 
 }
